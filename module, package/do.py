f40f1978e32188eb01a3ee418fcbaffc62b91041
Winni_Lina <9811011sa01@gmail.com>
2021-08-27 10:23:31 +0900

    package

---
+++ module, package/do.py
@@ -0,0 +1,24 @@
+# import my_module as mm
+# mm.지원이()
+# print(mm.date)
+#
+# import travel2.gangreung as ga
+# gp = ga.GangreungPackage()
+# print(gp)
+#
+# from travel2 import gangreung
+# gp = gangreung.GangreungPackage()
+# print(gp)
+#
+# from travel2.gangreung import GangreungPackage
+# gp = GangreungPackage()
+# print(gp)
+
+
+from travel2 import *
+ilsan.이지()
+gp = gangreung.GangreungPackage()
+print(gp)
+
+from travel2 import *
+ilsan.이지()
