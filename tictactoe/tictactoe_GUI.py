45189575a448e89fdedca19eabd13f5550844c91
User <w2027@e-mirin.hs.kr>
2021-11-05 10:19:07 +0900

    tictactoe GUI init GUI

---
+++ tictactoe/tictactoe_GUI.py
@@ -0,0 +1,22 @@
+from tictactoe_game_engine import TictactoeGameEngine
+import tkinter
+
+
+class TictactoeGUI:
+    def __init__(self):
+        self.game_engine = TictactoeGameEngine()
+
+    def init_GUI(self):
+        self.CANVAS_SIZE = 300
+        self.root = tkinter.Tk()
+        self.root.title('틱택토')
+        self.root.resizable(f'{self.CANVAS_SIZE}x{self.CANVAS_SIZE}')
+        self.root.resizable(width=False, height=False)
+
+    def click_handler(self, event):
+
+    def draw_board(self):
+        pass
+
+if __name__ == '__main__':
+    ttt_GUI = TictactoeGUI()
