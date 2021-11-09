a9d45b33189ca36291c9e319c8c134c32b080fcd
Winni_Lina <9811011sa01@gmail.com>
2021-11-09 11:53:06 +0900

    tictactoe GUI init GUI

--- tictactoe/tictactoe_GUI.py
+++ tictactoe/tictactoe_GUI.py
@@ -5,18 +5,33 @@ import tkinter
 class TictactoeGUI:
     def __init__(self):
         self.game_engine = TictactoeGameEngine()
+        self.init_GUI()
 
     def init_GUI(self):
         self.CANVAS_SIZE = 300
         self.root = tkinter.Tk()
         self.root.title('틱택토')
-        self.root.resizable(f'{self.CANVAS_SIZE}x{self.CANVAS_SIZE}')
+        self.root.geometry(f'{self.CANVAS_SIZE}x{self.CANVAS_SIZE}')
         self.root.resizable(width=False, height=False)
 
+        self.canvas = tkinter.Canvas(self.root, bg='white', width=self.CANVAS_SIZE, height=self.CANVAS_SIZE)
+        self.canvas.pack()
+
+        self.images = {}
+        self.images['X'] = tkinter.PhotoImage(file='X.gif')
+        self.images['O'] = tkinter.PhotoImage(file='O.gif')
+
+        self.canvas.bind('<Button-1>', self.click_handler)  #*** (괄호가 없을 떄 바로 실행 안됨)
+
+
+        self.root.mainloop()
+
     def click_handler(self, event):
-        pass
+        print('click')
+
     def draw_board(self):
         pass
+
 
 if __name__ == '__main__':
     ttt_GUI = TictactoeGUI()
